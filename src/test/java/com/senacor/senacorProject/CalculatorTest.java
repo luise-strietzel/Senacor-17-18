1103d392bebbb7d9a4a444f0699215595100c13f
karolinkliem <s0555039@htw-berlin.de>
2017-11-02 13:37:31 +0100

    Testklasse geändert

--- src/test/java/com/senacor/senacorProject/CalculatorTest.java
+++ src/test/java/com/senacor/senacorProject/CalculatorTest.java
@@ -9,8 +9,8 @@ public class CalculatorTest {
 
     @Test
     public void addTest(){
-        int result = sut.add(1,2);
+        int result = sut.add(5,5);
 
-        Assert.assertEquals("1+2 should be 3", 3, result);
+        Assert.assertEquals("5+5 should be 10", 10, result);
     }
 }
