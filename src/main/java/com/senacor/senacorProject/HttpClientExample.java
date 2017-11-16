d942a20780a0e4a8fce82931efa001bb2e55fec5
karolinkliem <s0555039@htw-berlin.de>
2017-11-16 14:00:15 +0100

    GET request

--- src/main/java/com/senacor/senacorProject/HttpClientExample.java
+++ src/main/java/com/senacor/senacorProject/HttpClientExample.java
@@ -5,7 +5,9 @@ import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
@@ -25,8 +27,41 @@ public class HttpClientExample {
 
         HttpClientExample http = new HttpClientExample();
 
+        System.out.println("Testing 1 - Send Http GET request");
+        http.sendGet();
+
         System.out.println("\nTesting - Send Http POST request");
         http.sendPost();
+
+    }
+
+    // HTTP GET request
+    private void sendGet() throws Exception {
+
+        String url = "http://ec2-18-194-12-73.eu-central-1.compute.amazonaws.com/api/program/ada";
+
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet(url);
+
+        // add request header
+        request.addHeader("User-Agent", USER_AGENT);
+
+        HttpResponse response = client.execute(request);
+
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " +
+                response.getStatusLine().getStatusCode());
+
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+
+        System.out.println(result.toString());
 
     }
 
@@ -39,9 +74,9 @@ public class HttpClientExample {
         HttpPost post = new HttpPost(url);
 
         // add header
-        post.setHeader("USER-AGENT", USER_AGENT );
-        post.addHeader("Authorization", "BASIC a2tiOnNlY3JldA==" );
-        post.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        post.addHeader("USER-AGENT", USER_AGENT );
+        post.addHeader("Authorization", "Basic a2tiOnNlY3JldA==" );
+        //post.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
         //add body
         List<NameValuePair> nameValuePairs = new ArrayList<>();
