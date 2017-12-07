7e14c84e25bbd99e35eb8bb84c7dc38256d4da5c
Mirvete <mirvete.rexha@hotmail.com>
2017-12-07 10:59:17 +0100

    Revert "Änderung"
    This reverts commit 748e4cbe72e124e6a2ba28addc447bf293fd8b05.

--- src/main/java/com/senacor/senacorProject/LambdaMethodHandler.java
+++ src/main/java/com/senacor/senacorProject/LambdaMethodHandler.java
@@ -1,5 +1,6 @@
 package com.senacor.senacorProject;
 
+import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
@@ -7,12 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-// RequestStreamHandler may be false, but a alexa known handler is required
 public class LambdaMethodHandler implements RequestStreamHandler {
-
-    public LambdaMethodHandler() {
-
-    }
 
     /**
      * Handles a Lambda Function request
@@ -25,7 +21,7 @@ public class LambdaMethodHandler implements RequestStreamHandler {
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
         context.getLogger().log("Input: " + input);
-        // TODO
+        output.write("hallo".getBytes());
     }
 
     public String handleRequest(String input, Context context) {
