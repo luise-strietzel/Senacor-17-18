e66b4028212283503436481fa30da65c06000764
Luise Strietzel <s0553358@student.htw-berlin.de>
2018-01-04 15:11:13 +0100

    Ãaenderungen in testOnIntentMethode

--- src/test/java/com/senacor/senacorProject/LBBSpeechletTest.java
+++ src/test/java/com/senacor/senacorProject/LBBSpeechletTest.java
@@ -14,6 +14,9 @@ public class LBBSpeechletTest {
 
     private LBBSpeechlet sut = new LBBSpeechlet();
 
+    private static final String INTENT_WHATSMYKONTOSTAND="Kontostand";
+    private static final String INTENT_WHATSMYLIMIT="Limit";
+
     @Test
     public void testOnLaunch() throws SpeechletException {
         String expected = "Willkommen im LBB-Konto. Fragen Sie mich nach Ihrem Kontostand mit dem Wort Kontostand.";
@@ -30,6 +33,44 @@ public class LBBSpeechletTest {
         PlainTextOutputSpeech result = (PlainTextOutputSpeech) speechletResponse.getOutputSpeech();
 
         Assert.assertEquals("Soll willkommenstext beinhalten", expected, result.getText());
+    }
+
+    @Test
+    public void testOnIntent(String intent) throws SpeechletException {
+        String expected = "Willkommen im LBB-Konto. Fragen Sie mich nach Ihrem Kontostand mit dem Wort Kontostand.";
+        LaunchRequest launchRequest = LaunchRequest.builder()
+                .withRequestId("123")
+                .withLocale(Locale.GERMANY)
+                .build();
+
+        Session session = Session.builder()
+                .withSessionId("23423")
+                .build();
+
+        SpeechletResponse speechletResponse = sut.onLaunch(launchRequest, session);
+        LBBSpeechlet mySpeechlet = new LBBSpeechlet();
+        GetCreditBalance creditBalance = new GetCreditBalance();
+        //System.out.println("wir testen die Methode handleKontostand");
+
+            //JsonElement myKontostand = myClient.sendGet(myClient.sendPost());
+            PlainTextOutputSpeech kontospeech = new PlainTextOutputSpeech();
+            kontospeech.setText("Ihr Kontostand beträgt "+ creditBalance.getcreditBalance() +" Euro. Vielen Dank, bis zum nächsten Mal.");
+
+        if(intent.equals(INTENT_WHATSMYLIMIT))
+        {
+            // springe in Methode handleLImit
+            Assert.assertEquals("Soll die handleLimit Methode auswählen", speech.getText(), mySpeechlet.handleKontostand(session));
+
+        }
+        else if(intent.equals(INTENT_WHATSMYKONTOSTAND))
+        {
+            //springe in Methode handleKontostand
+        }
+
+        //SpeechletResponse speechletResponse = sut.onLaunch(launchRequest, session);
+        //PlainTextOutputSpeech result = (PlainTextOutputSpeech) speechletResponse.getOutputSpeech();
+
+
     }
 
     @Test
