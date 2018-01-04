965747167fb6eeb8f2b93058b7856881f37de8f3
Mirvete <mirvete.rexha@hotmail.com>
2018-01-04 15:32:15 +0100

    Anderung im LBB-Speechlet

--- src/main/java/com/senacor/senacorProject/LBBSpeechlet.java
+++ src/main/java/com/senacor/senacorProject/LBBSpeechlet.java
@@ -11,13 +11,15 @@ public class LBBSpeechlet implements Speechlet {
     private static final Logger log = LoggerFactory.getLogger(LBBSpeechlet.class);
     private static final String INTENT_WHATSMYKONTOSTAND="Kontostand";
     private static final String INTENT_WHATSMYLIMIT="Limit";
-    private static final String INTENT_WHATSMYKONTOSTANDLIMIT="KONTO";
+    private static final String INTENT_WHATSMYKONTOSTANDLIMIT="Konto";
 
 
     public static void main(String[] args) throws Exception {
 
         LBBSpeechlet mySpeechlet = new LBBSpeechlet();
         mySpeechlet.handleKontostand();
+
+        mySpeechlet.handleKonto();
     }
 
 
@@ -42,19 +44,18 @@ public class LBBSpeechlet implements Speechlet {
         String intentName = request.getIntent().getName();
         if(INTENT_WHATSMYKONTOSTAND.equals(intentName))
         {
-         //   return handleKontostand(session);
+            //   return handleKontostand(session);
             return handleKontostand();
         }
         else if (INTENT_WHATSMYLIMIT.equals(intentName))
         {
             return handleLimit(session);
         }
-        else if (INTENT_WHATSMYKONTOSTANDLIMIT.equals(intentName));
+        else if (INTENT_WHATSMYKONTOSTANDLIMIT.equals(intentName))
         {
             return handleKonto();
         }
         else{
-
             throw new SpeechletException("Invalid Intent");
         }
     }
@@ -87,32 +88,34 @@ public class LBBSpeechlet implements Speechlet {
         return SpeechletResponse.newAskResponse(speech, createRepromptSpeech());
     }
 
-/*
-    private SpeechletResponse handleStopIntent() {
-        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-        speech.setText("auf wiedersehen.");
-        return SpeechletResponse.newTellResponse(speech);
-    }
+    /*
+        private SpeechletResponse handleStopIntent() {
+            PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+            speech.setText("auf wiedersehen.");
+            return SpeechletResponse.newTellResponse(speech);
+        }
 
-    private SpeechletResponse handleHelpIntent() {
-        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-        speech.setText("bestimme wieviel seiten dein würfel hat oder würfle");
-        return SpeechletResponse.newTellResponse(speech);
-    }
-    */
-private SpeechletResponse handleKonto() {
-    Konto konto = new Konto();
-    //System.out.println("wir testen die Methode handleKontostand");
-    try {
-        //JsonElement myKontostand = myClient.sendGet(myClient.sendPost());
-        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-        speech.setText("Ihr Kontostand beträgt "+ konto.getKontostand() +" Euro und ihr Limit " + konto.getKonto()+ " Euro. Vielen Dank, bis zum nächsten Mal.");
-        System.out.println(speech.getText());
-        return SpeechletResponse.newTellResponse(speech);
-    } catch (Exception e) {
-        e.printStackTrace();
-        return null;
+        private SpeechletResponse handleHelpIntent() {
+            PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+            speech.setText("bestimme wieviel seiten dein würfel hat oder würfle");
+            return SpeechletResponse.newTellResponse(speech);
+        }
+        */
+    private SpeechletResponse handleKonto(){
+        Konto konto=new Konto();
+        //System.out.println("wir testen die Methode handleKontostand");
+        try {
+            //JsonElement myKontostand = myClient.sendGet(myClient.sendPost());
+            PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+            speech.setText("Ihr Kontostand beträgt "+ konto.getKontostand() +" Euro und ihr Limit"+ konto.getKonto()+" Euro. Vielen Dank und bis zum nächsten Mal.");
+            System.out.println(speech.getText());
+            return SpeechletResponse.newTellResponse(speech);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
 
+    }
 
     private Reprompt createRepromptSpeech() {
         PlainTextOutputSpeech repromptSpeech = new PlainTextOutputSpeech();
